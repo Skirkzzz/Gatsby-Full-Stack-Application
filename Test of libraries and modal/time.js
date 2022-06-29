7a6ae507f29a5011e1d821aa709f663738225609
Your Namels <selina.kirk82@gmail.com>
2022-06-29 19:53:20 +0100

    Time stamp library

---
+++ Test of libraries and modal/time.js
@@ -0,0 +1,86 @@
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+import { round } from 'javascript-time-ago/steps'
+
+const timeStamp = getelementbyid(#jobPosted)
+
+TimeAgo.addDefaultLocale(en)
+
+const customLabels = {
+  second: {
+    past: {
+      one: "{0} second earlier",
+      other: "{0} seconds earlier"
+    },
+    future: {
+      one: "{0} second later",
+      other: "{0} seconds later"
+    }
+  },
+  ...
+  [
+    {
+      // "second" labels are used for formatting the output.
+      formatAs: 'second'
+    },
+    {
+      // This step is effective starting from 59.5 seconds.
+      minTime: 60,
+      // "minute" labels are used for formatting the output.
+      formatAs: 'minute'
+    },
+    {
+      // This step is effective starting from 59.5 minutes.
+      minTime: 60 * 60,
+      // "hour" labels are used for formatting the output.
+      formatAs: 'hour'
+    },
+    …
+  ]
+}
+
+
+
+TimeAgo.addLabels('en', 'custom', customLabels)
+
+timeAgo.format(Date.now() - 10 * 1000, customStyle)
+// "10 seconds earlier"
+
+getTimeToNextUpdate(
+    date: number, // The date argument, converted to a timestamp.
+    {
+      getTimeToNextUpdateForUnit(unit: string): number?,
+                         // Returns "time to next update" for a time unit.
+                         // This is what the library calls internally
+                         // when `formatAs` is configured for a `step`.
+                         // Example: `getTimeToNextUpdateForUnit('minute')`.
+                         // Can return `undefined` in edge cases:
+                         // for example, when `unit` is "now".
+  
+      now: number,       // The current date timestamp.
+  
+      future: boolean    // Is `true` if `date > now`, or if `date === now`
+                         // and `future: true` option was passed to `.format()`.
+    }
+  ): number?
+
+let updateTimer
+
+function render() {
+  // Format the date.
+  const [formattedDate, timeToNextUpdate] = timeAgo.format(date, {
+    getTimeToNextUpdate: true
+  })
+  // Update the label.
+  setFormattedDate(formattedDate)
+  // Schedule next render.
+  // `timeToNextUpdate` may be `undefined`, so provide a sensible default.
+  updateTimer = setTimeout(render, getSafeTimeoutInterval(timeToNextUpdate || 60 * 1000))
+}
+
+// `setTimeout()` has a bug where it fires immediately
+// when the interval is longer than about `24.85` days
+const SET_TIMEOUT_MAX_SAFE_INTERVAL = 2147483647
+function getSafeTimeoutInterval(interval) {
+  return Math.min(interval, SET_TIMEOUT_MAX_SAFE_INTERVAL)
+}
